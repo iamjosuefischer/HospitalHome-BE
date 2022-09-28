0bedf3281e9a2d2e7ce473d0b8292a5f2c999bd8
iamjosuefischer <josuefischercraft@gmail.com>
2022-09-28 13:49:09 -0500

    Revert "Complete back-end"
    This reverts commit fed577d333f4cff131e0e5296fc5209cd6100f30.

--- HospitalHomeBE/serializers/__init__.py
+++ HospitalHomeBE/serializers/__init__.py
@@ -1,3 +1,2 @@
 from .medicoSerializer import MedicoSerializer
 from .usuarioSerializer import UsuarioSerializer
-from .pacienteSerializer import PacienteSerializer
--- HospitalHomeBE/views/__init__.py
+++ HospitalHomeBE/views/__init__.py
@@ -1,4 +1,2 @@
 from .userView import UsuarioListView, UsuarioRetrieveUpdateDeleteView
 from .medicoView import MedicoListCreateView, MedicoRetrieveUpdateView
-from .pacienteView import createpaciente, detailpaciente
-from .userCreateView import usercreateview
